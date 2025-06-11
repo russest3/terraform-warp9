39c4a8fb249855d37bd330d61d28d5c19d71aaf9
russest3 <s.p.russell00@gmail.com>
2025-06-11 19:06:18 -0400

    updates

--- backend/backend.tf
+++ backend/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         key = "terraform/tfstate.tfstate"
         bucket = "tj-remote-backend-2020"
-        region = "eu-west-2"
+        region = "us-east-2"
         access_key = "your_access_key"
         secret_key = "your_secret_key"
     }
